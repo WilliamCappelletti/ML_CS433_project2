fb7f55b4a64a4eb177f11ecb14bebe9445d37cc1
WilliamCappelletti <cappelletti.william@gmail.com>
2018-11-29 14:25:18 +0100

    typo

--- src/general_code.py
+++ src/general_code.py
@@ -17,6 +17,7 @@ from pandas import DataFrame
 
 def cast_categories(frame, column, cat_name = 'Group'):
     '''Returns a new data frame in which the specified column has been changed by new colums representing the appertenance to one specified category'''
+    
     new_frame = frame.copy()
     categories = new_frame[column].unique()
     splitted_cols = {'{cat_name}_{x}'.format(cat_name=cat_name, x=x): [ 1 if inside else 0 for inside in new_frame[column]==x] for x in categories}
